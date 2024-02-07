a9fde450986cba965ab7040a091a466a7d91db76
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-02-07 17:53:58 +0000

    feat: Add HUD and shots count

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class BulletController : MonoBehaviour {
 
     void Start() {
-        
+        GameObject.Find("GameManager").GetComponent<GameManager>().shots++;
+        GameObject.Find("GameManager").GetComponent<GameManager>().UpdateShots();
     }
 
     void Update() {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     [SerializeField] Texture2D cursorTarget;
+    [SerializeField] TextMeshProUGUI shotsText;
+    public int shots = 0;
 
     void Start() {
         Vector2 hotspot = new Vector2 (cursorTarget.width/2, cursorTarget.height/2);
         Cursor.SetCursor(cursorTarget, hotspot, CursorMode.Auto);
+
+        UpdateShots();
     }
 
 
     void Update() {
         
     }
+
+    public void UpdateShots() {
+        shotsText.text = $"Disparos: {shots}";
 }
